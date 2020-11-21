9011e44fa71cd3d432e58bff35d9df2dc0b58bea
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2020-11-21 18:45:13 -0500

    config changes

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Corona
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.assets.enabled = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
